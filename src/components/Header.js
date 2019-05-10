694ebacdc1f95399ea7fa9122fdf741d1574c616
mjain13 <mayank.jain@merckgroup.com>
2019-05-10 20:43:31 +0530

    change Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,23 +7,17 @@ import read from '../image/ReadBook.png';
 class Header extends Component{
     render(){
         return(
-            <nav className={'navbar navbar-expand-lg  bg-light bg-dark'}>
+            <nav className={'navbar navbar-expand  bg-light bg-dark'}>
             <img src={icon} class="navbar-brand" alt={read}></img>
                 <NavLink 
-                    // to={'/'}
-                    // activeStyle ={{color:'white'}}
                     className={'navbar-brand href="#"'}>
                     {'Book Store'}
                 </NavLink>
-                <button className="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                 </button>
-                <div className={'collapse navbar-collapse '} id="navbarNav">
+                <div className={'row navbar'} id="navbarNav">
                     <ul className={'navbar-nav '}>
-                        <li className={'nav-item active'}>
+                        <li className={'row nav-item'}>
                             <NavLink to={'/Add'} className="nav-link" href="#" activeStyle ={{color:'green'}}>
                                 {' Add Books'}
-                                <span className="sr-only">(current)</span>
                             </NavLink>
                         </li>&emsp;
                         <li className={'nav-item'}>
@@ -31,11 +25,6 @@ class Header extends Component{
                                 {'Book List'}
                             </NavLink>
                         </li >&emsp;
-                        {/* <li className={'nav-item'} >
-                            <NavLink to={'/Details'} activeStyle ={{color:'green'}}>
-                            {'Details'}
-                            </NavLink>
-                        </li> */}
                     </ul>
                 </div>
             </nav>
--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 import { get, del,up } from '../action/books/';
 import ajax from '../utils/ajax';
+import {Modal} from 'react-bootstrap';
 
 import read from '../image/ReadBook.png';
 import ed1 from '../image/edit.png';
@@ -11,11 +12,29 @@ import del1 from '../image/delete.png';
 class List extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       books: [],
-      book_id: this.props.match.params.book_id
+      book_id: this.props.match.params.book_id,
+      show: false,
+      obj: []
     };
+    this.close = this.close.bind(this);
+    this.show= this.show.bind(this);
   };
+  show(ind1){
+    this.setState({
+      show: true, obj: ind1
+    })
+  };
+  close(){
+    this.setState({
+    show:false
+    });
+  };
+  onShow(ind1){
+    this.show(ind1);
+  }
   componentDidMount() {
       ajax({
       url: '/Books',
@@ -69,69 +88,80 @@ class List extends Component {
       <Fragment>
         <div
           className={'row'}>
+                           {/* ---------------  Loader --------------------------        */}
+        {books && !books.length &&<div className="text-center" >
+            <div class="spinner-border" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>}
           {books && books.map((books, k) =>
             <div
-              className={`col-md-6 mb-3 px-2`}key={k} >             
+              className={`col-md-6 mb-3 px-2`}key={k} > 
+                          
               <div
                 className={'card shadow'}>
                 <div
                   className={'card-body .bg-gradient-primary'}>
                   <h5
                     className={'card-header text-center'}>
-                    <img src={read} class="navbar-brand" alt={read}></img>
+                    <img src={read} className="navbar-brand" alt={read}></img>
                     {books.title}
                   </h5>
                   <h6
                     className={'card-body card-subtitle mb-2 text-muted text-center'}>
                     {`By `}<strong>{books.author}</strong>
                   </h6>
+                  <div>
                   <p
                     className={'card-text border-bottom mb-3 pb-2'}>
                     {books.description}
                   </p>
+                  </div>
                   <Link
-                    // onClick={this.onUp.bind(this, books.book_id)}
                     to={`/Edit/${books.book_id}`}
                     title={'Edit Book'}
                     className={'card-link '}>
-                    <img src={ed1} class="navbar-brand" alt={ed1}></img>
-                  </Link>
-
-                  {/* <button
-                    onClick={this.onDel.bind(this, books.book_id)}
-                    // to={`${books.book_id}`}
-                    className={'card-link text-danger float-center'}>
-                    <img src={del1} class="navbar-brand" alt={del1}></img>
-                    {'Delete'}
-                  </button> */}
-                  
+                    <img src={ed1} className="navbar-brand" alt={ed1}></img>
+                  </Link>                
                   <Link
                       to={`/List/${books.book_id}`}>
                     <div 
                       onClick={this.onDel.bind(this, books.book_id)}
                       title={'Delete Book'}
                       className={'card-link center d-inline-block'}>
-                        <img src={del1} class="navbar-brand" alt={del1}></img>
+                      <img src={del1} className="navbar-brand" alt={del1}></img>
                     </div>
                   </Link>
-                  <Link
-                    to={`View/${books.book_id}`}
-                    title={'View Book'}
-                    className={'card-link float-right'}>
-                    <img src={open} class="navbar-brand" alt={open}></img>
-                    {/* {'View'} */}
+                  <Link>
+                       {/* to={`/List/${books.book_id}`}> */}
+                    <div 
+                      onClick={this.onShow.bind(this, books)}
+                      title={'Open Book'}
+                      className={'card-link center d-inline-block'}>
+                      <img src={open} className="navbar-brand" alt={open}></img>
+                    </div>
                   </Link>
                 </div>
               </div>
             </div>
           )}
         </div>
-        {/* {books && !books.length && <li className={'list-group-item font-weight-bold text-center'}>{'Loading Books, Please wait ..'}</li>} */}
-        {books && !books.length &&<div className="spinner-border justify-content-center" role="status" >
-          <span className="sr-only">Loading...</span>
-        </div>}
-        {/* {books && !books.length && <li className={'list-group-item font-weight-bold text-center'}>{'No Product Added!'}</li>} */}
-      </Fragment>
+                {/* ----------------- Modal ---------------- */}
+        <form className={"form-group shadow p-3 mb-5 bg-light rounded"} >
+          <Modal show={this.state.show} onHide={this.close}>
+          <Modal.Header closeButton className="bg-primary">
+          <Modal.Title>Title: {this.state.obj.title}</Modal.Title>
+          </Modal.Header>
+          {this.state.obj !=null ? 
+          <Modal.Body>{this.state.obj.description}<br/><br/><div className={"text-right"}>
+          By :{this.state.obj.author} </div>
+          </Modal.Body> : 'No data'}
+          <Modal.Footer className="bg-secondary">
+          <button className={"btn btn-link-green"} onClick={this.close}><h6>Close</h6></button>
+          </Modal.Footer >
+          </Modal>
+        </form>
+    </Fragment>                    
     );
   }
 }
